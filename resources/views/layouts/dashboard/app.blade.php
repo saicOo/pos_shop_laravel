4438a471b404db40c5e96aa8c1c06c72aa29d3a0
unknown <sayed.ahmed.elbadry@gmail.com>
2023-03-22 17:11:25 -0700

    u

--- resources/views/layouts/dashboard/app.blade.php
+++ resources/views/layouts/dashboard/app.blade.php
@@ -168,10 +168,10 @@
 
     <footer class="main-footer">
         <div class="pull-right hidden-xs">
-            <b>Version</b> 2.4.0
+            <b>Version</b> 1.0.0
         </div>
-        <strong>Copyright &copy; 2014-2016
-            <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+        <strong>Copyright &copy; 2022-2023
+            <a href="https://saicopy.com">Saico</a>.</strong> All rights
         reserved.
     </footer>
 
